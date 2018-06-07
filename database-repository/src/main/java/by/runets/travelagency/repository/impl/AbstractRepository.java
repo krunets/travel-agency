c5e7239985cf66276aa5bd681c041a221a6d6a1a
Kiryl Runets <Kiryl_Runets@epam.com>
2018-06-07 17:12:16 +0300

    Add sql schema

---
+++ database-repository/src/main/java/by/runets/travelagency/repository/impl/AbstractRepository.java
@@ -0,0 +1,7 @@
+package by.runets.travelagency.repository.impl;
+
+/**
+ * Created by Kiryl_Runets on 6/7/2018.
+ */
+public class AbstractRepository {
+}
---
+++ database-repository/src/test/java/by/runets/travelagency/repository/Test.java
@@ -0,0 +1,7 @@
+package by.runets.travelagency.repository;
+
+/**
+ * Created by Kiryl_Runets on 6/7/2018.
+ */
+public class Test {
+}
