694908c25ef154f44446ca682a1767d2bc97ce4f
Kiryl Runets <Kiryl_Runets@epam.com>
2018-06-07 17:16:45 +0300

    database repository commit

--- database-repository/src/main/java/by/runets/travelagency/repository/IRepository.java
+++ database-repository/src/main/java/by/runets/travelagency/repository/IRepository.java
@@ -1,7 +1,18 @@
 package by.runets.travelagency.repository;
 
-/**
- * Created by Kiryl_Runets on 6/7/2018.
+import by.runets.travelagency.entity.Entity;
+
+import java.util.List;
+import java.util.Optional;
+
+/** Common interface which provides CRUD methods in repository layer.
+ * @param <T> is a generic param which must be inherited from Entity class.
+ * @param <K> is a generic param which represents a key param.
  */
-public interface IRepository {
+public interface IRepository<T extends Entity, K> {
+	void create(final T entity);
+	List<Optional<T>> readAll();
+	Optional<T> read(final K id);
+	void update(final T entity);
+	void delete(final T entity);
 }
--- database-repository/src/main/java/by/runets/travelagency/repository/impl/AbstractRepository.java
+++ database-repository/src/main/java/by/runets/travelagency/repository/impl/AbstractRepository.java
@@ -1,7 +1,34 @@
 package by.runets.travelagency.repository.impl;
 
-/**
- * Created by Kiryl_Runets on 6/7/2018.
- */
-public class AbstractRepository {
+import by.runets.travelagency.entity.Entity;
+import by.runets.travelagency.repository.IRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class AbstractRepository <T extends Entity, K>  implements IRepository<T, K> {
+	@Override
+	public void create (T entity) {
+		
+	}
+	
+	@Override
+	public List<Optional<T>> readAll () {
+		return null;
+	}
+	
+	@Override
+	public Optional<T> read (K id) {
+		return null;
+	}
+	
+	@Override
+	public void update (T entity) {
+		
+	}
+	
+	@Override
+	public void delete (T entity) {
+		
+	}
 }
