d958f50c041b69a93efba044075c04db45fd78ca
Kiryl_Runets <Kiryl_Runets@epam.com>
2018-06-10 18:28:07 +0300

    Add entity mappers

--- database-repository/src/main/java/by/runets/travelagency/mapper/CountryRowMapper.java
+++ database-repository/src/main/java/by/runets/travelagency/mapper/CountryRowMapper.java
@@ -9,35 +9,47 @@ import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CountryRowMapper implements RowMapper<Country> {
-	@Override
-	public Country mapRow (ResultSet resultSet, int i) throws SQLException {
-		Country<Integer> country = new Country<>();
-		
-		country.setId(resultSet.getInt("c_id"));
-		country.setName(resultSet.getString("c_name"));
-		
-		while (resultSet.next()) {
-			Hotel<Integer> hotel = new Hotel<>();
-			Tour<Integer> tour = new Tour<>();
-			
-			tour.setId(resultSet.getInt("t_id"));
-			tour.setPhoto(resultSet.getString("photo"));
-			tour.setDate(resultSet.getDate("date").toLocalDate());
-			tour.setDuration(Duration.ofDays(resultSet.getLong("duration")));
-			tour.setCost(resultSet.getBigDecimal("cost"));
-			tour.setTourType(TourType.getTypeByValue(resultSet.getString("t_type")));
-			
-			hotel.setId(resultSet.getInt("h_id"));
-			hotel.setName(resultSet.getString("h_name"));
-			hotel.setPhone(resultSet.getString("h_phone"));
-			hotel.setStars(resultSet.getInt("h_stars"));
-			hotel.setCountry(country);
-			
-			country.getHotels().add(hotel);
-		}
-		
-		return country;
-	}
+  @Override
+  public Country mapRow(ResultSet resultSet, int i) throws SQLException {
+    Country<Integer> country = new Country<>();
+
+    Set<Hotel<Integer>> hotelSet = new HashSet<>();
+    Set<Tour<Integer>> tourSet = new HashSet<>();
+
+    country.setId(resultSet.getInt("c_id"));
+    country.setName(resultSet.getString("c_name"));
+
+    while (resultSet.next()) {
+      Hotel<Integer> hotel = new Hotel<>();
+      Tour<Integer> tour = new Tour<>();
+
+      tour.setId(resultSet.getInt("t_id"));
+      tour.setPhoto(resultSet.getString("photo"));
+      tour.setDate(resultSet.getDate("date").toLocalDate());
+      tour.setDescription(resultSet.getString("description"));
+      tour.setDuration(Duration.ofDays(resultSet.getLong("duration")));
+      tour.setCost(resultSet.getBigDecimal("cost"));
+      tour.setTourType(TourType.getTypeByValue(resultSet.getString("t_type")));
+
+      tourSet.add(tour);
+
+      hotel.setId(resultSet.getInt("h_id"));
+      hotel.setName(resultSet.getString("h_name"));
+      hotel.setPhone(resultSet.getString("h_phone"));
+      hotel.setStars(resultSet.getInt("h_stars"));
+      hotel.setCountry(country);
+
+      hotelSet.add(hotel);
+    }
+
+    country.setHotels(new ArrayList<>(hotelSet));
+    country.setTours(new ArrayList<>(tourSet));
+
+    return country;
+  }
 }
---
+++ database-repository/src/main/java/by/runets/travelagency/mapper/ReviewRowMapper.java
@@ -0,0 +1,28 @@
+package by.runets.travelagency.mapper;
+
+import by.runets.travelagency.entity.Review;
+import by.runets.travelagency.entity.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class ReviewRowMapper implements RowMapper<Review> {
+  @Override
+  public Review mapRow(ResultSet resultSet, int i) throws SQLException {
+	Review<Integer> review = new Review<>();
+	User<Integer> user = new User<>();
+
+	review.setId(resultSet.getInt("r_id"));
+    review.setContent(resultSet.getString("content"));
+
+    user.setId(resultSet.getInt("u_id"));
+    user.setLogin(resultSet.getString("login"));
+    user.setPassword(resultSet.getString("password"));
+
+    review.setUser(user);
+
+    return review;
+  }
+}
