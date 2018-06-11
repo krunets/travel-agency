06c6e4dd594236e5dc5a6810a7e5306fa18e7959
Kiryl Runets <Kiryl_Runets@epam.com>
2018-06-11 16:51:06 +0300

    Fix mappers

--- database-repository/src/main/java/by/runets/travelagency/mapper/ReviewRowMapper.java
+++ database-repository/src/main/java/by/runets/travelagency/mapper/ReviewRowMapper.java
@@ -9,20 +9,20 @@ import java.sql.SQLException;
 
 
 public class ReviewRowMapper implements RowMapper<Review> {
-  @Override
-  public Review mapRow(ResultSet resultSet, int i) throws SQLException {
-	Review<Integer> review = new Review<>();
-	User<Integer> user = new User<>();
-
-	review.setId(resultSet.getInt("r_id"));
-    review.setContent(resultSet.getString("content"));
-
-    user.setId(resultSet.getInt("u_id"));
-    user.setLogin(resultSet.getString("login"));
-    user.setPassword(resultSet.getString("password"));
-
-    review.setUser(user);
-
-    return review;
-  }
+	@Override
+	public Review mapRow (ResultSet resultSet, int i) throws SQLException {
+		Review<Integer> review = new Review<>();
+		User<Integer> user = new User<>();
+		
+		review.setId(resultSet.getInt("r_id"));
+		review.setContent(resultSet.getString("content"));
+		
+		user.setId(resultSet.getInt("u_id"));
+		user.setLogin(resultSet.getString("login"));
+		user.setPassword(resultSet.getString("password"));
+		
+		review.setUser(user);
+		
+		return review;
+	}
 }
--- database-repository/src/main/java/by/runets/travelagency/mapper/TourRowMapper.java
+++ database-repository/src/main/java/by/runets/travelagency/mapper/TourRowMapper.java
@@ -4,6 +4,7 @@ import by.runets.travelagency.entity.Country;
 import by.runets.travelagency.entity.Tour;
 import by.runets.travelagency.entity.TourType;
 import by.runets.travelagency.entity.User;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
@@ -14,37 +15,37 @@ import java.util.List;
 
 
 public class TourRowMapper implements RowMapper<Tour> {
-  @Override
-  public Tour mapRow(ResultSet resultSet, int i) throws SQLException {
-    Tour<Integer> tour = new Tour<>();
-    List<User<Integer>> users = new ArrayList<>();
-    List<Country<Integer>> countries = new ArrayList<>();
-
-    tour.setId(resultSet.getInt("t_id"));
-    tour.setPhoto(resultSet.getString("photo"));
-    tour.setDate(resultSet.getDate("date").toLocalDate());
-    tour.setDescription(resultSet.getString("description"));
-    tour.setDuration(Duration.ofDays(resultSet.getLong("duration")));
-    tour.setCost(resultSet.getBigDecimal("cost"));
-    tour.setTourType(TourType.getTypeByValue(resultSet.getString("t_type")));
-
-    while (resultSet.next()) {
-      User<Integer> user = new User<>();
-      Country<Integer> country = new Country<>();
-
-      user.setId(resultSet.getInt("u_id"));
-      user.setLogin(resultSet.getString("login"));
-      user.setPassword(resultSet.getString("password"));
-      users.add(user);
-
-      country.setId(resultSet.getInt("c_id"));
-      country.setName(resultSet.getString("c_name"));
-      countries.add(country);
-    }
-
-    tour.setUsers(users);
-    tour.setCountries(countries);
-
-	return tour;
-  }
+	@Override
+	public Tour mapRow (ResultSet resultSet, int i) throws SQLException {
+		Tour<Integer> tour = new Tour<>();
+		List<User<Integer>> users = new ArrayList<>();
+		List<Country<Integer>> countries = new ArrayList<>();
+		
+		tour.setId(resultSet.getInt("t_id"));
+		tour.setPhoto(resultSet.getString("photo"));
+		tour.setDate(resultSet.getDate("date").toLocalDate());
+		tour.setDescription(resultSet.getString("description"));
+		tour.setDuration(Duration.ofDays(resultSet.getLong("duration")));
+		tour.setCost(resultSet.getBigDecimal("cost"));
+		tour.setTourType(TourType.getTypeByValue(resultSet.getString("t_type")));
+		
+		
+		User<Integer> user = new User<>();
+		Country<Integer> country = new Country<>();
+		
+		user.setId(resultSet.getInt("u_id"));
+		user.setLogin(resultSet.getString("login"));
+		user.setPassword(resultSet.getString("password"));
+		users.add(user);
+		
+		country.setId(resultSet.getInt("c_id"));
+		country.setName(resultSet.getString("c_name"));
+		countries.add(country);
+		
+		tour.setUsers(users);
+		tour.setCountries(countries);
+		
+		
+		return tour;
+	}
 }
