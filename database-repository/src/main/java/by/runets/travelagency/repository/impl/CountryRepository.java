d814c9002ca8d93e1860e241a6b0884ded0baae4
Kiryl Runets <kiryl_runets@epam.com>
2018-06-19 13:55:11 +0300

    refactoring pre step

--- database-repository/src/main/java/by/runets/travelagency/repository/impl/CountryRepository.java
+++ database-repository/src/main/java/by/runets/travelagency/repository/impl/CountryRepository.java
@@ -6,28 +6,31 @@ import by.runets.travelagency.joiner.Joiner;
 import by.runets.travelagency.mapper.CountryRowMapper;
 import by.runets.travelagency.repository.IRepository;
 import lombok.AllArgsConstructor;
+import org.omg.CORBA.Object;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
 public class CountryRepository implements IRepository<Country, Integer> {
-	private final JdbcTemplate jdbcTemplate;
+	private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	private final Joiner<Country> joiner;
 	
 	@Override
 	public void create (final Country entity) {
-		jdbcTemplate.update(CountryQuery.INSERT_INTO_COUNTRY, entity.getId(), entity.getName());
+		Map<String, java.lang.Object> parameters = new HashMap<>();
+		parameters.put("c_id", entity.getId());
+		parameters.put("c_name", entity.getName());
+		namedParameterJdbcTemplate.update(CountryQuery.INSERT_INTO_COUNTRY, parameters);
 	}
 	
 	@Override
 	public List<Optional<Country>> readAll () {
 		try {
-			List<Country> countries = jdbcTemplate.query(CountryQuery.READ_ALL_COUNTRY, new CountryRowMapper());
+			List<Country> countries = namedParameterJdbcTemplate.query(CountryQuery.READ_ALL_COUNTRY, new CountryRowMapper());
 			return joiner.join(countries).stream()
 					.map(Optional::ofNullable)
 					.collect(Collectors.toList());
@@ -39,7 +42,7 @@ public class CountryRepository implements IRepository<Country, Integer> {
 	@Override
 	public Optional<Country> read (final Integer id) {
 		try {
-			List<Country> countries = jdbcTemplate.query(CountryQuery.READ_COUNTRY_BY_ID, new Object[]{id}, new CountryRowMapper());
+			List<Country> countries = namedParameterJdbcTemplate.query(CountryQuery.READ_COUNTRY_BY_ID, new Object[]{id}, new CountryRowMapper());
 			List<Country> result = joiner.join(countries);
 			return !result.isEmpty()
 					? Optional.ofNullable(result.get(0))
@@ -51,13 +54,20 @@ public class CountryRepository implements IRepository<Country, Integer> {
 	
 	@Override
 	public void update (final Country entity) {
-		jdbcTemplate.update(CountryQuery.UPDATE_COUNTRY_BY_ID, entity.getName(), entity.getId());
+		Map<String, java.lang.Object> parameters = new HashMap<>();
+		parameters.put("c_id", entity.getId());
+		parameters.put("c_name", entity.getName());
+		namedParameterJdbcTemplate.update(CountryQuery.UPDATE_COUNTRY_BY_ID, parameters);
 	}
 	
 	@Override
 	public void delete (final Country entity) {
-		jdbcTemplate.update(CountryQuery.DELETE_M2M_COUNTRY, entity.getId());
-		jdbcTemplate.update(CountryQuery.DELETE_HOTEL_CONSTRAINT, entity.getId());
-		jdbcTemplate.update(CountryQuery.DELETE_COUNTRY_BY_ID, entity.getId());
+		Map<String, java.lang.Object> parameters = new HashMap<>();
+		parameters.put("c_id", entity.getId());
+		parameters.put("c_name", entity.getName());
+		namedParameterJdbcTemplate.update(CountryQuery.DELETE_M2M_COUNTRY, parameters);
+		namedParameterJdbcTemplate.update(CountryQuery.DELETE_HOTEL_CONSTRAINT, parameters;
+		namedParameterJdbcTemplate.update(CountryQuery.DELETE_COUNTRY_BY_ID, parameters);
 	}
+	
 }
