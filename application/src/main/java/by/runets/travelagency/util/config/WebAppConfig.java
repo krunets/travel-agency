a9b4fde3f395619d83b385e2618d2924bee89192
Kiryl_Runets <Kiryl_Runets@epam.com>
2018-08-20 18:57:42 +0300

    commit with config troubles

--- application/src/main/java/by/runets/travelagency/util/config/WebAppConfig.java
+++ application/src/main/java/by/runets/travelagency/util/config/WebAppConfig.java
@@ -1,12 +1,33 @@
 package by.runets.travelagency.util.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "by.runets.travelagency")
-public class WebAppConfig {
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+	
+	
+	@Bean
+	public FreeMarkerViewResolver freeMarkerViewResolver() {
+		FreeMarkerViewResolver freeMarkerViewResolver = new FreeMarkerViewResolver();
+		freeMarkerViewResolver.setCache(true);
+		freeMarkerViewResolver.setSuffix(".ftl");
+		freeMarkerViewResolver.setPrefix("");
+		return freeMarkerViewResolver;
+	}
+	
+	@Bean
+	public FreeMarkerConfigurer freemarkerConfig() {
+		FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+		freeMarkerConfigurer.setTemplateLoaderPath("/WEB-INF/views/");
+		return freeMarkerConfigurer;
+	}
 }
